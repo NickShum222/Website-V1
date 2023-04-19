a5f0e13b544032d6efa932198c3dfab37e5c1842
NickShum <115332882+NickShum222@users.noreply.github.com>
2023-04-18 20:02:46 -0400

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,22 @@
 import "../styles/globals.css";
-
+import { Analytics } from "@vercel/analytics/react";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-<link href="https://fonts.googleapis.com/css2?family=PT+Mono&display=swap" rel="stylesheet"/>
-        
-
-<script src="node_modules/@material-tailwind/html@latest/scripts/ripple.js"></script>
-
-<script src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Mono&display=swap"
+          rel="stylesheet"
+        />
+        <script src="node_modules/@material-tailwind/html@latest/scripts/ripple.js"></script>
+        <script src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
