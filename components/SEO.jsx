8f6e5ab1c28bca8fe62e2bf329e8a5a705cd1351
NickShum <115332882+NickShum222@users.noreply.github.com>
2023-05-09 11:52:19 -0400

    update experience/project

---
+++ components/SEO.jsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+const SEO = () => {
+  return (
+    <>
+      <Head>
+        <title>Nick Shum</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="Nick Shum | Website Portfolio" />
+        <meta name="author" content="Nick Shum" />
+        <meta name="theme-color" content="#151515" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:type" content="website" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+    </>
+  );
+};
+
+export default SEO;
