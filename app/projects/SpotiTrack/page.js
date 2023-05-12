bba7f687e6ea4b185c173fdeab6591517a2d4868
NickShum <115332882+NickShum222@users.noreply.github.com>
2023-05-12 12:12:15 -0400

    fixed tablet responsive

--- app/projects/SpotiTrack/page.js
+++ app/projects/SpotiTrack/page.js
@@ -1,8 +1,11 @@
 "use client";
 import { Button } from "@material-tailwind/react";
+import SEO from "@/components/SEO";
 const page = () => {
   return (
-    <div className="bg-grey h-[100vh] w-full flex flex-col justify-center items-center lg:py-[50px] py-[20px]">
+    <>
+    <SEO/>
+     <div className="bg-grey h-[100vh] w-full flex flex-col justify-center items-center lg:py-[50px] py-[20px]">
       <a href="/">
         <Button
           className="rounded-none normal-case lg:py-3 lg:px-8 py-2 md:px-6 px-4  mb-5 text-[14px] md:text-[16px] lg:text-[18px]  text-highlight transition-all hover:bg-highlight/10 active:bg-highlight/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none border-highlight border-[1px]"
@@ -15,6 +18,8 @@ const page = () => {
         <source src="/SpotiTrackDemo.mp4" />
       </video>
     </div>
+    </>
+   
   );
 };
 
