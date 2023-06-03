3e81b65cdbfd88e541bf2f90e5d5ebf82230773c
NickShum <115332882+NickShum222@users.noreply.github.com>
2023-06-03 14:56:55 -0400

    update landing

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -6,7 +6,6 @@ import { styles } from "@/styles";
 import { ThemeProvider, Button } from "@material-tailwind/react";
 
 const Hero = () => {
-
   return (
     <section
       id="home"
@@ -32,16 +31,16 @@ const Hero = () => {
           >
             Software Developer
           </motion.h2>
-       
+
           <motion.div
             variants={slideUp(1)}
             className="flex flex-row items-center justify-center gap-5 ml-1 "
           >
-               <a href="/resume">
-            <Button className="rounded-none normal-case py-3 px-8 text-[14px] md:text-[16px] lg:text-[18px]  text-highlight transition-all hover:bg-highlight/10 active:bg-highlight/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none border-highlight border-[1px]">
-              Resume
-            </Button>
-          </a>
+            <a href="/resume">
+              <Button className="rounded-none normal-case py-3 px-8 text-[14px] md:text-[16px] lg:text-[18px]  text-highlight transition-all hover:bg-highlight/10 active:bg-highlight/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none border-highlight border-[1px]">
+                Resume
+              </Button>
+            </a>
             <Button
               className="rounded-none normal-case py-3 px-8 font-normal font-mono text-[14px] md:text-[16px] lg:text-[18px]   text-highlight transition-all hover:bg-highlight/10 active:bg-highlight/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none  "
               data-ripple-light="true"
@@ -50,7 +49,13 @@ const Hero = () => {
             </Button>
           </motion.div>
         </div>
-        <motion.div className="top-[0] lg:w-[90%] w-[120%] mx-[-30%] sm:mt-[-5%] inline">
+        <motion.div
+          variants={staggerContainer}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: false, amount: 0.2 }}
+          className="top-[0] lg:w-[90%] w-[120%] mx-[-30%] sm:mt-[-5%] inline"
+        >
           <Moon />
         </motion.div>
       </motion.div>
